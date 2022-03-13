a73c8e61af871a78a1a4f4e4c774ef6c9f493767
Hossein AliHosseini <themmdhossein@gmail.com>
2022-03-13 16:52:11 +0330

    Delete cryptoexchangecompare directory

--- cryptoexchangecompare/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cryptoexchangecompare.settings')
-
-app = Celery('cryptoexchangecompare')
-
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
