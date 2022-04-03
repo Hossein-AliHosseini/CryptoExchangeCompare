eb6c2a9cf5e9d1cd9da0cc4078b125aaad8710a8
Hossein-AliHosseini <themmdhossein@gmail.com>
2022-04-03 12:52:06 +0430

    Final Rebase

--- cryptoexchangecompare/settings/base.py
+++ cryptoexchangecompare/settings/base.py
@@ -97,9 +97,9 @@ AUTHENTICATION_BACKENDS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.ce.sharif.edu'
-EMAIL_HOST_USER = 'alihosseini@ce.sharif.edu'
-EMAIL_HOST_PASSWORD = 'qweasdzxc1'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = '587'
 
 
